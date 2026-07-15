e13b57c07c8421f78a1a37353fcd2e58362ecadb
AzanTechSolution <azantechsolutions@gmail.com>
2026-07-15 13:16:15 +0500

    New Fix

--- js/gallery-data.js
+++ js/gallery-data.js
@@ -1,2 +1,153 @@
 window.galleryYears = ["All", "2026", "2025", "2024", "2023", "2022", "2021"];
-window.galleryData = [];
+window.galleryData = [
+  {
+    "year": "2026",
+    "src": "../public/2026/Facilities1.webp",
+    "type": "image",
+    "filename": "Facilities1.webp"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/Facilities2.webp",
+    "type": "image",
+    "filename": "Facilities2.webp"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-17.jpg",
+    "type": "image",
+    "filename": "gallery-17.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-16.jpg",
+    "type": "image",
+    "filename": "gallery-16.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-15.jpg",
+    "type": "image",
+    "filename": "gallery-15.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-14.jpg",
+    "type": "image",
+    "filename": "gallery-14.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-13.jpg",
+    "type": "image",
+    "filename": "gallery-13.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-12.jpg",
+    "type": "image",
+    "filename": "gallery-12.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-11.jpg",
+    "type": "image",
+    "filename": "gallery-11.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-10.jpg",
+    "type": "image",
+    "filename": "gallery-10.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-9.jpg",
+    "type": "image",
+    "filename": "gallery-9.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-8.jpg",
+    "type": "image",
+    "filename": "gallery-8.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-7.jpg",
+    "type": "image",
+    "filename": "gallery-7.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/Dr Iqra checking.jpg",
+    "type": "image",
+    "filename": "Dr Iqra checking.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/eye-care.jpg",
+    "type": "image",
+    "filename": "eye-care.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/eye-care-checkup.jpg",
+    "type": "image",
+    "filename": "eye-care-checkup.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-overviews.jpg",
+    "type": "image",
+    "filename": "gallery-overviews.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-about.jpg",
+    "type": "image",
+    "filename": "gallery-about.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-overview.jpeg",
+    "type": "image",
+    "filename": "gallery-overview.jpeg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-6.jpg",
+    "type": "image",
+    "filename": "gallery-6.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-5.jpg",
+    "type": "image",
+    "filename": "gallery-5.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/Staff-team.jpg",
+    "type": "image",
+    "filename": "Staff-team.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-3.jpg",
+    "type": "image",
+    "filename": "gallery-3.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-2.jpg",
+    "type": "image",
+    "filename": "gallery-2.jpg"
+  },
+  {
+    "year": "2026",
+    "src": "../public/2026/gallery-1.jpg",
+    "type": "image",
+    "filename": "gallery-1.jpg"
+  }
+];
